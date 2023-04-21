8cdcbfd2d269129711941c62cfc4ccb0f62f5ffe
azawert <53710085+azawert@users.noreply.github.com>
2023-04-21 18:56:31 +0300

    добавил функционал для логирования прохождения тренировки

--- workout-backend/app/workout/workout.routes.ts
+++ workout-backend/app/workout/workout.routes.ts
@@ -3,6 +3,11 @@ import express from 'express'
 import { isAuth } from '../middleware/auth.middleware'
 
 import {
+	createNewWorkoutLog,
+	getWorkoutLogById,
+	updateCompleteWorkoutLog
+} from './log/workout-log.controller'
+import {
 	createWorkout,
 	deleteWorkout,
 	getAllWorkouts,
@@ -14,8 +19,11 @@ const router = express.Router()
 
 router.get('/', isAuth, getAllWorkouts)
 router.get('/:id', isAuth, getSingleWorkout)
+router.get('/log/:id', isAuth, getWorkoutLogById)
 router.post('/', isAuth, createWorkout)
+router.post('/log/:workoutId', isAuth, createNewWorkoutLog)
 router.put('/:id', isAuth, updateWorkout)
+router.patch('/log/complete/:id', isAuth, updateCompleteWorkoutLog)
 router.delete('/:id', isAuth, deleteWorkout)
 
 export default router
